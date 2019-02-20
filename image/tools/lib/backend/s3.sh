095f47d7b2806a36e3c05471273a6c1e3ac0f431
odra <lrossett@redhat.com>
2019-02-20 12:58:32 +0000

    initital code

---
+++ image/tools/lib/backend/s3.sh
@@ -0,0 +1,5 @@
+#required env vars: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_S3_BUCKET_NAME
+function upload_archive {
+    archive_path="$1"
+    echo "TODO: upload archive \"$archive_path\" using s3 cli"
+}
