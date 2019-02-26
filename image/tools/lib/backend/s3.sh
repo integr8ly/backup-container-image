ce9b16e5b551b6d35ffbd6492bb7a2160736117d
odra <lrossett@redhat.com>
2019-02-26 16:36:53 +0000

    AWS_S3_BUCKET_SUFFIX env var

--- image/tools/lib/backend/s3.sh
+++ image/tools/lib/backend/s3.sh
@@ -1,8 +1,14 @@
 #required env vars: AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_S3_BUCKET_NAME
+#optional env vars: AWS_S3_BUCKET_SUFFIX
 function upload_archive {
     file_list=$1
     datestamp=$2
-    bucket_folder=$3
+
+    if [[ -n "$AWS_S3_BUCKET_SUFFIX" ]]; then
+        bucket_folder="$3/$AWS_S3_BUCKET_SUFFIX"
+    else 
+        bucket_folder=$3
+    fi
 
     for fname in "$file_list"; do
         s3cmd put --progress $fname "s3://$AWS_S3_BUCKET_NAME/$bucket_folder/$datestamp/$(basename $fname)"
