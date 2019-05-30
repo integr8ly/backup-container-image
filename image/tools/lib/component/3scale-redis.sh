a8e66d1fbc1dd67113c01c206a55dc7f77a3feb0
Leonardo Rossetti <lrossett@redhat.com>
2019-05-30 09:07:53 +0100

    backend redis bgsave

--- image/tools/lib/component/3scale-redis.sh
+++ image/tools/lib/component/3scale-redis.sh
@@ -1,9 +1,25 @@
+function get_redis_host {
+    echo "`oc get secret ${COMPONENT_SECRET_NAME} -n ${COMPONENT_SECRET_NAMESPACE} -o jsonpath={.data.REDIS_HOST} | base64 --decode`"
+}
+
+function save {
+    local host="$(get_redis_host)"
+    local lastsave="$(redis-cli -h ${host} lastsave)"
+    redis-cli -h ${host} bgsave
+
+    while [ "$lastsave" -eq "$(redis-cli -h ${host} lastsave)" ]; do
+        sleep 5
+    done
+}
+
 function component_dump_data {
+    save
     local ts=$(date '+%H_%M_%S')
     dest_file="$1/archives/dump-${ts}.rdb"
     dump_rdb_path="/var/lib/redis/data/dump.rdb"
 
     oc projects
     redis_pod_name=$(oc get pods -l deploymentConfig=backend-redis -o name -n ${PRODUCT_NAMESPACE_PREFIX}3scale | sed -e 's/^pod\///')
+
     cp_pod_data "${PRODUCT_NAMESPACE_PREFIX}3scale/${redis_pod_name}:${dump_rdb_path}" "${dest_file}"
 }
